3cefc3ab0d64f925117110a884d366f59a5a0ea4
Sharm-Dev <sharmainetaylor@ymail.com>
2022-01-13 20:11:30 +0000

    add concat message

---
+++ write-strine-message.js
@@ -0,0 +1,9 @@
+let username = "per"
+let message = "You have tree new notifications"
+
+console.log(message + ", " + username + "!")
+
+// Create a variable, messageToUser, that contains the message we have logged
+
+let messageToUser = `${message}, ${username}!`;
+console.log(messageToUser);
