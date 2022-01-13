bbb43f36ad1be41b964b513bd5f7d86734236de2
Sharm-Dev <sharmainetaylor@ymail.com>
2022-01-13 20:18:02 +0000

    add greeting, used variables and template literals

---
+++ write-greeting-string-message.js
@@ -0,0 +1,10 @@
+// Create two variables, name and greeting. The name variable should store your name,
+// and the greeting should store e.g. "Hi, my name is "
+
+// Create a third variable, myGreeting, that contatenates the two strings
+// Log myGreeting to the console
+
+let greeting = "Jason";
+let name = "Hi, my name is";
+let myGreeting = `${name} ${greeting}`;
+console.log(myGreeting);
