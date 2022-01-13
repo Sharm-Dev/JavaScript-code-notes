735846d8e9468c83a863b1a8ce196e6557027d54
Sharm-Dev <sharmainetaylor@ymail.com>
2022-01-13 20:25:42 +0000

    add answers to what the console would log out if it was running

---
+++ what-would-it-log-out.js
@@ -0,0 +1,13 @@
+
+//What would the console log out????????
+
+console.log(4 + 5); // 9
+console.log("2" + "4"); // "24"
+console.log("5" + 1); // "51"
+console.log(100 + "100"); // "100100"
+
+
+
+
+
+
