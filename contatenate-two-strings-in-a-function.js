aa6b4679fc80f929ffaebe3032fe5911af931601
Sharm-Dev <sharmainetaylor@ymail.com>
2022-01-14 19:36:16 +0000

    add a function that logs out variables when called.

---
+++ contatenate-two-strings-in-a-function.js
@@ -0,0 +1,10 @@
+let name = "Linda";
+let greeting = "Hi there";
+
+// Create a function that logs out "Hi there, Linda!" when called
+function hi() {
+  let fullGreeting = `${greeting}, ${name}!`;
+  console.log(fullGreeting);
+}
+
+hi();
