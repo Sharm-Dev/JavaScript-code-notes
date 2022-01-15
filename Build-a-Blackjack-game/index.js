331d8eba9911a06e0ed4b0e24fd709d2a2404236
Sharm-Dev <sharmainetaylor@ymail.com>
2022-01-15 01:25:46 +0000

    add if statement to log outcome results in the console.

--- Build-a-Blackjack-game/index.js
+++ Build-a-Blackjack-game/index.js
@@ -2,7 +2,15 @@
 // Set their values to a random number between 2-11
 let firstCard = Math.floor(Math.random() * 11) + 2;
 let secondCard = Math.floor(Math.random() * 11) + 2;
-
 // 2. Create a variable, sum, and set it to the sum of the two cards
 let sum = firstCard + secondCard;
 console.log(sum);
+
+
+if (sum < 21) {
+    console.log("Do you want to draw a new card? 🙂");
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳");
+} else {
+    console.log("You're out of the game! 😭");
+}
