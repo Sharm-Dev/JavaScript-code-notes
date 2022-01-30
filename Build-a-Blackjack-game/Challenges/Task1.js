25a30b39655fe0a365cc4acdd7f7d57bb56a4f3a
Sharm-Dev <sharmainetaylor@ymail.com>
2022-01-30 15:45:16 +0000

    true or false quiz

---
+++ Build-a-Blackjack-game/Challenges/Task1.js
@@ -0,0 +1,9 @@
+// Write either true or false and check it after on the console
+
+console.log(4 === 3); // false
+console.log(5 > 2); // true
+console.log(12 > 12); // false
+console.log(3 < 0); // false
+console.log(3 >= 3); // true
+console.log(11 <= 11); // true
+console.log(3 <= 2); // false
--- Build-a-Blackjack-game/index.js
+++ Build-a-Blackjack-game/index.js
@@ -1,16 +1,27 @@
 // 1. Create two variables, firstCard and secondCard.
 // Set their values to a random number between 2-11
+
 let firstCard = Math.floor(Math.random() * 11) + 2;
 let secondCard = Math.floor(Math.random() * 11) + 2;
+
+// let firstCard = 10;
+// let secondCard = 10;
+
 // 2. Create a variable, sum, and set it to the sum of the two cards
 let sum = firstCard + secondCard;
 console.log(sum);
+// 1. Create a variable called isAlive and assign it to true
+let isAlive = true;
 
 
 if (sum < 21) {
     console.log("Do you want to draw a new card? 🙂");
+    isAlive = false
 } else if (sum === 21) {
     console.log("Wohoo! You've got Blackjack! 🥳");
 } else {
     console.log("You're out of the game! 😭");
+    isAlive = false;
 }
+
+console.log(isAlive);
