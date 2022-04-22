c77ea80e578fb21a82cf909db7fe3c7901c8cbb8
Sharmaine <sharmainetaylor@ymail.com>
2022-04-22 20:00:23 +0100

    Added if statments to check if the sum is at a certain amount and if it is, it will display a message on the webpage that fits that condition

--- Build-a-Blackjack-game/index.js
+++ Build-a-Blackjack-game/index.js
@@ -1,27 +1,68 @@
-// 1. Create two variables, firstCard and secondCard.
-// Set their values to a random number between 2-11
 
-let firstCard = Math.floor(Math.random() * 11) + 2;
-let secondCard = Math.floor(Math.random() * 11) + 2;
+  // 1. Create two variables, firstCard and secondCard.
+  // Set their values to a random number between 2-11
 
-// let firstCard = 10;
-// let secondCard = 10;
+  let firstCard = Math.floor(Math.random() * 11) + 2;
+  let secondCard = Math.floor(Math.random() * 11) + 2;
 
-// 2. Create a variable, sum, and set it to the sum of the two cards
-let sum = firstCard + secondCard;
-console.log(sum);
-// 1. Create a variable called isAlive and assign it to true
-let isAlive = true;
+  let cardsEl = document.getElementById("cards");
+  let textToAdd = document.createTextNode(
+    `Cards: ${firstCard} + ${secondCard}`
+  );
+  cardsEl.appendChild(textToAdd);
+
+  // Create a variable, sum, and set it to the sum of the two cards
+  let sum = firstCard + secondCard;
+//   console.log(sum);
+
+  let sumEl = document.getElementById("sum");
+  let sumToAdd = document.createTextNode(`Sum total: ${sum}`);
+  sumEl.appendChild(sumToAdd);
+
+  let hasBlackJack = false;
+  // Create a variable called isAlive and assign it to true
+  let isAlive = true;
+  // Declare a variable called message and assign its value to an empty string
+let message = ""
+
+let messageEl = document.getElementById("message-el");
+console.log(messageEl, "<------- messageEl");
+  
+function startGame() {
+    if (sum < 21) {
+        message = "Do you want to draw a new card? 🙂";
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game! 😭";
+        isAlive = false;
+    }
+    messageEl.textContent = message;
+};
 
 
-if (sum < 21) {
-    console.log("Do you want to draw a new card? 🙂");
-    isAlive = false
-} else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳");
-} else {
-    console.log("You're out of the game! 😭");
-    isAlive = false;
-}
 
-console.log(isAlive);
+// let resultEl = document.getElementById("result");
+// let resultToAdd = document.createTextNode(`${message}`);
+// resultEl.appendChild(resultToAdd);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("Has blackjack: " + hasBlackJack);
+// console.log("Player still live: " + isAlive);
+// console.log(message);
