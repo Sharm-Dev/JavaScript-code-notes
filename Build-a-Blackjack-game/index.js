e19c56575acbf2f2122d5e94cd65b43a750da777
Sharmaine <sharmainetaylor@ymail.com>
2022-04-25 21:51:30 +0100

    Created a card array and called the cards in the renderGame function

--- Build-a-Blackjack-game/index.js
+++ Build-a-Blackjack-game/index.js
@@ -1,7 +1,9 @@
 let firstCard = Math.floor(Math.random() * 11) + 2;
 let secondCard = Math.floor(Math.random() * 11) + 2;
 let thirdCard = Math.floor(Math.random() * 11) + 2; 
-let card = [firstCard, secondCard] 
+// 1. Create a new array - cards - that contains firstCard and secondCard
+let card = [firstCard, secondCard, thirdCard]; 
+//
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
@@ -19,24 +21,24 @@ function startGame() {
 
 
 function renderGame() {
+  // 2. Refer to the cards array when rendering out the cards
   sumEl.textContent = `Sum: ${sum}`;
   cardsEl.textContent = `Cards: ${card[0]} + ${card[1]}`;
 
-    if (sum < 21) {
-        message = "Do you want to draw a new card?";
-    } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack!";
-        hasBlackJack = true;
-    } else {
-        message = "You're out of the game!";
-        isAlive = false;
-    }
-    messageEl.textContent = message;
+  if (sum < 21) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
 };
 
 function newCard() {
-  card += thirdCard;
-  sum += thirdCard;
+  sum += card[2];
   renderGame();
   
 }
