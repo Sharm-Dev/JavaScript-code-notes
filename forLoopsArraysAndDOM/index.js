a5dc7f899d77d9c803cdf9fbd8552e0bc9b87f03
Sharmaine <sharmainetaylor@ymail.com>
2022-07-01 22:38:35 +0100

    removed unwanted symbol

--- forLoopsArraysAndDOM/index.js
+++ forLoopsArraysAndDOM/index.js
@@ -3,5 +3,5 @@ let greetingEl = document.getElementById("greeting-el");
 
 // Render the sentence in the greetingEl paragraph using a for loop and .textContent
 for (let i = 0; i < sentence.length; i++){
-    greetingEl.textContent += sentence[i] += " "
+    greetingEl.textContent += sentence[i] + " "
 }
