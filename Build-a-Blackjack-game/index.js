ccb1ffc81b1be9344ad8868d18411bcac08b066c
Sharmaine <sharmainetaylor@ymail.com>
2022-07-01 22:56:47 +0100

    Created a for loop that renders out all the cards instead of just two

--- Build-a-Blackjack-game/index.js
+++ Build-a-Blackjack-game/index.js
@@ -1,6 +1,6 @@
 let firstCard = Math.floor(Math.random() * 11) + 2;
 let secondCard = Math.floor(Math.random() * 11) + 2;
-let card = [firstCard, secondCard]; 
+let cards = [firstCard, secondCard]; 
 let sum = firstCard + secondCard;
 
 let hasBlackJack = false;
@@ -21,7 +21,11 @@ function startGame() {
 function renderGame() {
   // 2. Refer to the cards array when rendering out the cards
   sumEl.textContent = `Sum: ${sum}`;
-  cardsEl.textContent = `Cards: ${card[0]} + ${card[1]}`;
+  cardsEl.textContent = "Cards: ";
+  // Create a for loop that renders out all the cards instead of just two
+  for (let i = 0; i < cards.length; i++){
+    cardsEl.textContent += cards[i] + " "
+  }
 
   if (sum < 21) {
     message = "Do you want to draw a new card?";
@@ -38,9 +42,9 @@ function renderGame() {
 function newCard() {
   let thirdCard = Math.floor(Math.random() * 11) + 2;
   // Push the card to the cards array
-  card.push(thirdCard);
-  sum += card[2];
-  console.log(card);
+  cards.push(thirdCard);
+  sum += thirdCard;
+  console.log(cards);
   renderGame();
 }
 
